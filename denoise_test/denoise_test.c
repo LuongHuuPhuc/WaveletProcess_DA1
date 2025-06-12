ea859304fcf95784e99816e6a7be30442709c8e3
Luong Huu Phuc <Huuphuc1016@gmail.com>
2025-06-12 18:15:19 +0700

    Do some CmakeList.txt

---
+++ denoise_test/denoise_test.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <wavefunc.h>
+#include <wavelib.h>
+#include <wauxlib.h>
+
+/**
+ * @note Muc dich cua su dung DWT la:
+ * \note - Khu nhieu 
+ * \note - Trich xuat dac trung 
+ * \note - Nen du lieu khi can truyen tai qua mang (IoT, BLE, Wifi)
+ * \note - Phat hien dieu kien y sinh (Dinh R trong ECG, S1-S2 trong PCG, Bien dong tin hieu huyet ap trong PPG)
+ */
+wave_object wave_obj;
+
+denoise_object denoise_obj;
+
+static FILE *file_path = "D:/C-C++_project/WaveletProcess/data_text";
+
+int main(void){
+
+}
